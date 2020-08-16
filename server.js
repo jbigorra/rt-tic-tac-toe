6f7e977664d0d04f02b7db94e08c1ffdf1459c14
Juan Bigorra <juanchoibf@gmail.com>
2020-08-17 00:53:14 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+app
+  .set('view engine', 'ejs')
+  .set('views', path.join(__dirname, '/src/views'))
+  .use(helmet())
+  .use(cors())
+  .use(morgan('common'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
