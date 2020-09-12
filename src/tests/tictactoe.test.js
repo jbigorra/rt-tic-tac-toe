10ef77f176300d53101c6a9fa07394b2152b19c0
Juan Bigorra <juanchoibf@gmail.com>
2020-09-13 01:07:09 +0200

    Add test cases to evaluate game completion

--- src/tests/tictactoe.test.js
+++ src/tests/tictactoe.test.js
@@ -3,7 +3,7 @@ describe('Tic-tac-toe', () => {
     const playerOneId = 'player1';
     const playerTwoId = 'player2';
 
-    it('start with empty board and two players and player 1 as current player', () => {
+    it('Start with empty board and two players and player 1 as current player', () => {
       const match = new TicTacToe(playerOneId, playerTwoId);
       const emptyBoard = [['', '', ''], ['', '', ''], ['', '', '']];
 
@@ -13,7 +13,7 @@ describe('Tic-tac-toe', () => {
       expect(match.playerInTurn.id).toBe(playerOneId);
     });
 
-    it('let play player 1 turn and switch playerInTurn to be player 2', () => {
+    it('Let play player 1 turn and switch playerInTurn to be player 2', () => {
       const match = new TicTacToe(playerOneId, playerTwoId);
       const boardWithOneturnPlayed = [['', 'X', ''], ['', '', ''], ['', '', '']];
 
@@ -23,30 +23,30 @@ describe('Tic-tac-toe', () => {
       expect(match.playerInTurn.id).toBe(playerTwoId);
     });
 
-    it('let player 2 play 2nd turn and switch playerInTurn to be player 1', () => {
+    it('Let player 2 play 2nd turn and switch playerInTurn to be player 1', () => {
       const match = new TicTacToe(playerOneId, playerTwoId);
       match.playTurn(playerOneId, { x: 1, y: 0 });
-      const boardWithTwoturnPlayed = [['', 'X', ''], ['', '', ''], ['', '', 'O']];
+      const boardWithTwoturnsPlayed = [['', 'X', ''], ['', '', ''], ['', '', 'O']];
 
       match.playTurn(playerTwoId, { x: 2, y: 2 });
 
-      expect(match.board).toMatchObject(boardWithTwoturnPlayed);
+      expect(match.board).toMatchObject(boardWithTwoturnsPlayed);
       expect(match.playerInTurn.id).toBe(playerOneId);
     });
 
-    it('let player 1 play 3rd turn and switch playerInTurn to be player 2', () => {
+    it('Let player 1 play 3rd turn and switch playerInTurn to be player 2', () => {
       const match = new TicTacToe(playerOneId, playerTwoId);
       match.playTurn(playerOneId, { x: 1, y: 0 });
       match.playTurn(playerTwoId, { x: 2, y: 2 });
-      const boardWithTwoturnPlayed = [['', 'X', 'X'], ['', '', ''], ['', '', 'O']];
+      const boardWithThreeturnsPlayed = [['', 'X', 'X'], ['', '', ''], ['', '', 'O']];
 
       match.playTurn(playerOneId, { x: 2, y: 0 });
 
-      expect(match.board).toMatchObject(boardWithTwoturnPlayed);
+      expect(match.board).toMatchObject(boardWithThreeturnsPlayed);
       expect(match.playerInTurn.id).toBe(playerTwoId);
     });
 
-    it('throw if playerId does not match player in turn', () => {
+    it('Throw if playerId does not match player in turn', () => {
       const match = new TicTacToe(playerOneId, playerTwoId);
 
       expect(() => {
@@ -56,17 +56,48 @@ describe('Tic-tac-toe', () => {
       expect(match.board).toMatchObject(emptyBoard);
       expect(match.playerInTurn.id).toBe(playerOneId);
     });
+
+    it('Finish when some row has the same values', () => {
+      const board = [['', 'X', 'X'], ['', '', ''], ['0', '', 'O']];
+      const match = new TicTacToe(playerOneId, playerTwoId, board);
+
+      match.playTurn(playerOneId, { x: 0, y: 0 });
+
+      const expectedBoardAfterWin = [['X', 'X', 'X'], ['', '', ''], ['0', '', 'O']];
+      expect(match.board).toMatchObject(expectedBoardAfterWin);
+      expect(match.completed).toBeTruthy();
+      expect(match.winner.id).toBe(playerOneId);
+    });
+
+    it('Finish when some colum has the same values', () => {
+      const board = [['X', '', 'O'], ['X', '', ''], ['', '', 'O']];
+      const match = new TicTacToe(playerOneId, playerTwoId, board);
+
+      match.playTurn(playerOneId, { x: 0, y: 2 });
+
+      const expectedBoardAfterWin = [['X', '', 'O'], ['X', '', ''], ['X', '', 'O']];
+      expect(match.board).toMatchObject(expectedBoardAfterWin);
+      expect(match.completed).toBeTruthy();
+      expect(match.winner.id).toBe(playerOneId);
+    });
   });
 });
 
+// Add test case for when the board is full and the game is finished without a winner\
+// Add test case for diagonals
+
 const emptyBoard = [['', '', ''], ['', '', ''], ['', '', '']];
+const xSymbol = 'X';
+const oSymbol = 'O';
 
 class TicTacToe {
-  constructor (playerOneId, playerTwoId) {
-    this.board = emptyBoard;
-    this.playerOne = { id: playerOneId, symbol: 'X' };
-    this.playerTwo = { id: playerTwoId, symbol: 'O' };
+  constructor (playerOneId, playerTwoId, boardWithProgress) {
+    this.board = boardWithProgress || emptyBoard;
+    this.playerOne = { id: playerOneId, symbol: xSymbol };
+    this.playerTwo = { id: playerTwoId, symbol: oSymbol };
     this.playerInTurn = this.playerOne;
+    this.winner = null;
+    this.completed = this.evaluateGameCompletion();
   }
 
   playTurn (playerId, playerMove = {}) {
@@ -75,7 +106,59 @@ class TicTacToe {
     const { x, y } = playerMove;
 
     this.board[y][x] = this.playerInTurn.symbol;
+    this.evaluateGameCompletion(this.playerInTurn);
+
     this.playerInTurn = this.playerInTurn.id === this.playerOne.id ? this.playerTwo : this.playerOne;
+  }
+
+  evaluateGameCompletion (player) {
+    if (this.rowHasSameSymbol() || this.columHasSameSymbol() || this.diagonalHasSameSymbol()) {
+      this.completed = true;
+      this.winner = player;
+      return;
+    }
+
+    if (this.boardIsFull()) {
+      this.completed = true;
+    }
+  }
+
+  rowHasSameSymbol () {
+    const rowHasSameSymbol = (row) => {
+      const firstValue = row[0];
+      return row.every(value => value === firstValue);
+    };
+
+    return this.board.some(rowHasSameSymbol);
+  }
+
+  columHasSameSymbol () {
+    let hasValidColumn = false;
+    for (let x = 0; x < this.board.length; x++) {
+      const initialValue = this.board[x][0];
+      for (let y = 0; y < this.board.length; y++) {
+        const value = this.board[y][x];
+        if (initialValue !== value) {
+          break;
+        }
+        if (y === this.board.length - 1) hasValidColumn = true;
+      }
+    }
+
+    return hasValidColumn;
+  }
+
+  diagonalHasSameSymbol () {
+    return false;
+  }
+
+  boardIsFull () {
+    return false;
+  }
+
+  _setPlayerInTurn (playerId) {
+    const symbol = playerId === this.playerOne.id ? this.playerOne.symbol : this.playerTwo.symbol;
+    this.playerInTurn = { id: playerId, symbol };
   }
 }
 
